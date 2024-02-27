b42111d6cc55753533fcd409184f4b2ed84c2d91
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2024-02-28 00:57:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/*Declare 3 variables in one statement
+var num1 num2 num3
+*/
+
+/*
+Declare 5 legal variable
+var john,num,$abc,_abd,num2
+
+& 5 illegal variable names
+var #john,-num,2abd,+num2
+*/
+var nam =
+  "Rules for naming JS variables\nVariable names can only contain alphabets, $,underscore\n Variable names are case sensitive\n Variable names should not be JS ";
+document.write(nam);
